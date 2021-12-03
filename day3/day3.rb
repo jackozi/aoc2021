e79b7ec9394be588959459250aec9b89b2a8d643
Jacko Zuidema <jacko@moneybird.com>
2021-12-03 08:55:58 +0100

    day3_2

--- day3/day3.rb
+++ day3/day3.rb
@@ -2,21 +2,39 @@ class Submarine
   attr_accessor :data
 
   def initialize(data:)
-    @data = data
+    @data = data.split.map(&:chars)
   end
 
-  def transposed_data
-    @data.split.map(&:chars).transpose
+  def gamma(data: @data)
+    data.transpose.map {|bit| bit.count("1") >= bit.count("0") ? "1" : "0" }.join
   end
 
-  def gamma
-    transposed_data.map {|bit| bit.count("1") > bit.count("0") ? "1" : "0" }.join.to_i(2)
+  def epsilon(data: @data)
+    data.transpose.map {|bit| bit.count("1") < bit.count("0") ? "1" : "0" }.join
   end
 
-  def epsilon
-    gamma ^ 0xfff
+  def oxygen
+    data = @data.dup
+    data.first.count.times do |i|
+      gamma = gamma(data: data)
+      data = data.filter {|entry| entry[i].eql?(gamma[i]) }
+    end
+
+    data.first.join.to_i(2)
+  end
+
+  def co2
+    data = @data.dup
+    data.first.count.times do |i|
+      epsilon = epsilon(data: data)
+      data = data.filter {|entry| entry[i].eql?(epsilon[i]) }
+
+      break if data.count == 1
+    end
+
+    data.first.join.to_i(2)
   end
 end
 
 submarine = Submarine.new(data: File.read('input.txt'))
-puts submarine.gamma * submarine.epsilon
+puts submarine.oxygen * submarine.co2
